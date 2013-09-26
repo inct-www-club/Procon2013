b58ab0b22eea295c218efa15d68b3b22591ecd88
Fumiaki Kinoshita <fumiexcel@gmail.com>
2013-09-26 17:43:27 +0900

    update preview.hs

--- preview.hs
+++ preview.hs
@@ -1,10 +1,8 @@
-{-# LANGUAGE TemplateHaskell, FlexibleContexts, RankNTypes, LambdaCase #-}
+{-# LANGUAGE TemplateHaskell, FlexibleContexts, RankNTypes, LambdaCase, MultiWayIf #-}
 {-
-
-cabal update
-cabal install free-game lens
+Requires GHC >= 7.6
+cabal update && cabal install free-game lens
 cat /path/to/text | encoder encode | runhaskell preview.hs
-
 -}
 import Graphics.UI.FreeGame
 import Control.Monad
@@ -15,6 +13,7 @@ import System.Random
 import Control.Monad.Trans.Maybe
 import Control.Lens
 import Control.Concurrent.MVar
+import System.Environment
 
 loadBitmaps "images"
 
@@ -25,6 +24,13 @@ die 'R' = fromBitmap _die_1_png
 die 'W' = fromBitmap _die_2_png
 die 'B' = fromBitmap _die_5_png
 
+dieMedium :: Picture2D m => Char -> m ()
+dieMedium = scale (10/16) . die
+
+sizeLarge = 96
+
+sizeMedium = 96 * 10 / 16
+
 data Packet = Packet {
     _meta :: String
     , _dice :: [Char]
@@ -34,6 +40,8 @@ makeLenses ''Packet
 data World = World {
     _packetIndex :: Int
     , _packets :: [Packet]
+    , _keyL :: Bool
+    , _keyR :: Bool
     }
 makeLenses ''World
 
@@ -48,11 +56,11 @@ renderPacket pkt = scale 0.7 $ translate (V2 48 48) $ flip evalStateT (view dice
         forM_ [0..4] $ \r -> do
             forM_ [0..8] $ \c -> do
                 x <- pickHead
-                translate (V2 (c * 96) (r * 96)) (die x)
+                translate (V2 c r ^* sizeLarge) (die x)
         forM_ [0..1] $ \r -> do
             forM_ [0..13] $ \c -> do
                 x <- pickHead
-                translate (V2 (c * 96 * 10 / 16 - 18) (r * 96 * 10 / 16 + 480 - 18)) $ scale (10/16) (die x)
+                translate (V2 c r ^* sizeMedium - V2 18 18 + V2 0 480) $ dieMedium x
     return ()
 
 renderWorld :: (MonadState World m, Picture2D m, MonadIO m) => m ()
@@ -71,8 +79,29 @@ stateToMVar mv m = do
     liftIO $ putMVar mv s'
     return a
 
-main = do
+previewMain window = do
     pkts <- toPackets <$> getLine
-    mv <- newMVar $ World { _packetIndex = 0, _packets = pkts }
-    runGame def $ do
-        foreverTick $ stateToMVar mv renderWorld
+    font <- loadFont "VL-PGothic-Regular.ttf"
+    runGame def { _windowed = window } $ flip execStateT (World { _packetIndex = 0, _packets = pkts, _keyL = False, _keyR = False }) $ foreverTick $ do
+        whenM (notM (use keyL) <&=> keySpecial KeyLeft) $ packetIndex -= 1
+        whenM (notM (use keyR) <&=> keySpecial KeyRight) $ packetIndex += 1
+        keyL <~ keySpecial KeyLeft
+        keyR <~ keySpecial KeyRight
+        renderWorld
+
+        i <- use packetIndex
+        translate (V2 100 240) $ colored (blue & _Alpha .~ 0.5) $ text font 240 $ "#" ++ show i
+
+        whenM (keySpecial KeyEsc) quit `asTypeOf` return ()
+
+main = do
+    args <- getArgs
+    if  | "-h" `elem` args -> putStrLn "\
+            \Usage: preview [-f] [-h]\n\
+            \\tstdin: a sequence of dice ([RWS]*)\n\
+            \\t-f Run in a full-screen mode\n\
+            \\t-h Show this message\n\n\
+            \\tPrevious packet: Left\n\
+            \\tNext packet: Right\n\
+            \\tQuit: Esc"
+        | otherwise -> previewMain ("-f" `notElem` args) >> return ()
