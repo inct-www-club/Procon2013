5c6391c59e6c072b7d9c87e3f0106db91f2a5f73
39mogura <kuroganemogura@docomo.ne.jp>
2013-10-08 17:14:08 +0900

    Color Pointing and Center Pointing

--- ui/procon/Result.h
+++ ui/procon/Result.h
@@ -50,17 +50,19 @@ public:
 		ResultDraw = false;
 	};
 
-	void Result_Set(std::vector<std::pair<Coord, int>> result, Point PacketLeftTop){
+	void Result_Set(std::vector<std::pair<s3d::Rect, int>> result, Point PacketLeftTop){
 		for(int i = 0; i < 5; i++){
 			for(int j = 0; j < 9; j++){
 				BigDice[i][j].SetDie(result[i*9 + j].second, 
-					Point(result[i*9 + j].first.first/2 + PacketLeftTop.x -50, result[i*9 + j].first.second/2), Point(0,0));
+					Point(result[i*9 + j].first.tl.x/2 + PacketLeftTop.x -50, result[i*9 + j].first.tl.y/2),
+					Point(result[i*9 + j].first.br.x/2 + PacketLeftTop.x -50, result[i*9 + j].first.br.y/2));
 			}
 		}
 		for(int i = 0; i < 2; i++){
 			for(int j = 0; j < 14; j++){
-				SmallDice[i][j].SetDie(result[i*14 + j + 45].second, 
-					Point(result[i*14 + j + 45].first.first/2 + PacketLeftTop.x -50, result[i*14 + j + 45].first.second/2), Point(0,0));
+				SmallDice[i][j].SetDie(result[i*14 + j + 45].second,
+					Point(result[i*14 + j + 45].first.tl.x/2 + PacketLeftTop.x -50, result[i*14 + j + 45].first.tl.y/2),
+					Point(result[i*14 + j + 45].first.br.x/2 + PacketLeftTop.x -50, result[i*14 + j + 45].first.br.y/2) );
 			}
 		}
 		ResultDraw = true;
@@ -76,6 +78,25 @@ public:
 			for(int i = 0; i < 2; i++){
 				for(int j = 0; j < 14; j++){
 					SmallDice[i][j].DrawRoll();
+				}
+			}
+
+			for(int i = 0; i < 5; i++){
+				for(int j = 0; j < 9; j++){
+					//Rect(BigDice[i][j].LeftTop,  BigDice[i][j].RightBottom).drawFrame(2,0,Palette::Orange);
+					int xsize = BigDice[i][j].RightBottom.x - BigDice[i][j].LeftTop.x,
+						ysize = BigDice[i][j].RightBottom.y - BigDice[i][j].LeftTop.y;
+						
+					Circle(Point(BigDice[i][j].LeftTop.x + xsize/2,  BigDice[i][j].LeftTop.y + ysize/2), 3).draw(Palette::Orange);
+				}
+			}
+			for(int i = 0; i < 2; i++){
+				for(int j = 0; j < 14; j++){
+					int xsize = SmallDice[i][j].RightBottom.x - SmallDice[i][j].LeftTop.x,
+						ysize = SmallDice[i][j].RightBottom.y - SmallDice[i][j].LeftTop.y;
+						
+					Circle(Point(SmallDice[i][j].LeftTop.x + xsize/2,  SmallDice[i][j].LeftTop.y + ysize/2), 3).draw(Palette::Orange);
+					//Rect(SmallDice[i][j].LeftTop,  SmallDice[i][j].RightBottom).drawFrame(2,0,Palette::Orange);
 				}
 			}
 		}
